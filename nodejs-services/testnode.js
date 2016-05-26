5be94147514ecf287e78e5986224e23cf7812f9e
PRINYA <prinya@throughwave.co.th>
2016-05-26 09:24:43 +0700

    Use bcrypt when login

--- nodejs-services/testnode.js
+++ nodejs-services/testnode.js
@@ -3,13 +3,15 @@ var express = require('express');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-var Client = require('mariasql');
-var c = new Client({
+var MariaClient = require('mariasql');
+var mariadbClient = new MariaClient({
   host: '127.0.0.1',
   user: 'root',
   password: '1prinya;',
   db: 'hct_database'
 });
+
+var bcrypt = require('bcrypt');
 
 // Add headers
 app.use(function (req, res, next) {
@@ -38,7 +40,7 @@ var server = app.listen(8081, function () {
 
   console.log("Example app listening at http://%s:%s", host, port)
 
-})
+});
 
 app.get('/listExamHistory', function (req, res) {
   var courseType = req.query['courseType'];
@@ -46,7 +48,7 @@ app.get('/listExamHistory', function (req, res) {
   if (courseType == '1') extendedQueryStr = ' WHERE course_type = "1"';
   if (courseType == '2') extendedQueryStr = ' WHERE course_type = "2"';
 
-  c.query('SELECT * FROM exam_history' + extendedQueryStr,
+  mariadbClient.query('SELECT * FROM exam_history' + extendedQueryStr,
           function(err, rows) {
     if (err)
       throw err;
@@ -81,11 +83,11 @@ app.get('/listExamHistory', function (req, res) {
     str = '[' + str + ']';
     res.end(str);
   });
-  c.end();
-})
+  mariadbClient.end();
+});
 
 app.get('/addExamHistory', function (req, res) {
-  c.query('INSERT INTO exam_history ' +
+  mariadbClient.query('INSERT INTO exam_history ' +
           '(fullname, citizen_id, exam_number, exam_time, exam_score, course_type, exam_datetime)' +
           ' VALUES (:param1, :param2, :param3, :param4, :param5, :param6, :param7)',
           {param1: 'สมศักดิ์ ค้าแก้ว', param2: '123456789', param3: '34', param4: 45, param5:47, param6:1, param7: '2016-05-23 08:30:01'},
@@ -98,8 +100,8 @@ app.get('/addExamHistory', function (req, res) {
     console.dir(str);
     res.end(str);
   });
-c.end();
-})
+  mariadbClient.end();
+});
 
 app.post('/login', function (req, res) {
   var username = req.body.username_param;
@@ -107,7 +109,7 @@ app.post('/login', function (req, res) {
 
   console.dir('User='+username+'  Pass='+password);
 
-  c.query('SELECT * FROM all_users ' +
+  mariadbClient.query('SELECT * FROM all_users ' +
           ' WHERE username = :param1',
           {param1: username},
           function(err, rows) {
@@ -115,13 +117,38 @@ app.post('/login', function (req, res) {
       throw err;
 
     if (rows.length > 0) {
-      res.end('SUCCESS');
+      var hashedPassword = rows[0].password;
+      bcrypt.compare(password, hashedPassword, function(err, result) {
+        if (result == true) {
+          res.end('SUCCESS');
+        } else {
+          res.end('FAIL');
+        }
+      });
     } else {
       res.end('FAIL');
     }
-
-    var str = '';
-    //res.end(str);
   });
-  c.end();
-})
+  mariadbClient.end();
+});
+
+app.post('/createUser', function (req, res) {
+  var username = req.body.username_param;
+  var password = req.body.password_param;
+  var saltRounds = 10;
+
+  bcrypt.hash(password, saltRounds, function(err, hashedPassword) {
+    console.dir('User='+username+'  Pass='+password+ '  Bcrypt='+hashedPassword);
+
+    mariadbClient.query('INSERT INTO all_users (username, password)' +
+            ' VALUES (:param1, :param2)',
+            {param1: username, param2: hashedPassword},
+            function(err, rows) {
+      if (err)
+        throw err;
+
+        res.end('SUCCESS');
+    });
+    mariadbClient.end();
+  });
+});
