662e4c890e213b23810d3fbdd479168b0366e609
PRINYA <prinya@throughwave.co.th>
2016-11-17 20:06:07 +0700

    Change some text Fix Access-Control-Allow_Origins

--- nodejs-services/testnode.js
+++ nodejs-services/testnode.js
@@ -18,7 +18,12 @@ var bcrypt = require('bcrypt');
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://128.199.95.79');
+    var allowedOrigins = ['http://main.clickeexam.in', 'http://hct.clickeexam.in',
+                          'http://abc.clickeexam.in', 'http://xyz.clickeexam.in'];
+    var origin = req.headers.origin;
+    if (allowedOrigins.indexOf(origin) > -1) {
+      res.setHeader('Access-Control-Allow-Origin', origin);  
+    }
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
